cc4fe04545d71056dab8ef68090ef47178f33daf
Himanshu Khanna <>
2021-09-04 21:42:13 +0530

    created igw and rt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-
+region = "us-east-2"
 #aws configure command
 }
 
@@ -16,10 +16,29 @@ resource "aws_vpc" "myapp-vpc" {
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.dev-vpc.id
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
       "Name" = "${var.env_prefix}-subnet-1"
+    }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+      "Name" = "${var.env_prefix}-rtb"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+      "Name" = "${var.env_prefix}-igw"
     }
 }
