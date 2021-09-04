4f600b007ebe39dbe95e8b5de2019b059df7e8bc
Himanshu Khanna <>
2021-09-05 00:38:22 +0530

    added ec2

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable "subnet_cidr_block" {}
 variable "avail_zone" {} #set TF_VAR_avail_zone="us-east-2a"
 variable "env_prefix" {}
 variable "my_ip" {}
+variable "instance_type" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -88,5 +89,33 @@ resource "aws_default_security_group" "default-sg" {
     
     tags = {
       "Name" = "${var.env_prefix}-default-sg"
+    }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+}
+
+/*output "aws-ami-id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}*/
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = "myapp-server-key-pair.pem"
+
+    tags = {
+      "Name" = "${var.env_prefix}-server"
     }
 }
