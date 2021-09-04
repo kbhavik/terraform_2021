5f3820782b311ac43567f6540a5c0fb35493f65e
Himanshu Khanna <>
2021-09-04 18:23:45 +0530

    creating demo project

--- main.tf
+++ main.tf
@@ -1,41 +1,25 @@
 provider "aws" {
-    region = "us-east-2"
+
 #aws configure command
 }
 
-variable "cidr_blocks" {
-    description = "cidr block for vpc and subnets"
-    default = [
-        {cidr_block = "10.0.0.0/16", name = "dev-vpc"},
-        {cidr_block = "10.0.10.0/24", name = "dev-subnet-1"}
-    ]
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
-}
-
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
 variable "avail_zone" {} #set TF_VAR_avail_zone="us-east-2a"
-resource "aws_vpc" "dev-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+variable "env_prefix" {}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-      "Name" = var.cidr_blocks[0].name
+      "Name" = "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
+resource "aws_subnet" "myapp-subnet-1" {
     vpc_id = aws_vpc.dev-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
+    cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
-      "Name" = var.cidr_blocks[1].name
+      "Name" = "${var.env_prefix}-subnet-1"
     }
-}
-
-output "dev-vpc-id" {
-    value = aws_vpc.dev-vpc.id
-}
-
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
 }
