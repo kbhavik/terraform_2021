8080942fb5b29336164ffc0e83da15d51d1488cf
Himanshu Khanna <>
2021-09-09 23:54:34 +0530

    added and configured module subnet

--- main.tf
+++ main.tf
@@ -10,38 +10,14 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-      "Name" = "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_default_route_table" "main-rtb" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-      "Name" = "${var.env_prefix}-main-rtb"
-    }
 }
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-      "Name" = "${var.env_prefix}-igw"
-    }
-}
-
-/*resource "aws_route_table_association" "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}*/
 
 resource "aws_default_security_group" "default-sg" {
     vpc_id = aws_vpc.myapp-vpc.id
---
+++ modules/subnet/variables.tf
@@ -0,0 +1,5 @@
+variable "subnet_cidr_block" {}
+variable "avail_zone" {} #set TF_VAR_avail_zone="us-east-2a"
+variable "env_prefix" {}
+variable "vpc_id" {}
+variable "default_route_table_id" {}
